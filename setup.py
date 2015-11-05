ceb243d0131c7f92e6cc52a3a6c4876987f9a3b3
JuanPablo <jpabloaj@gmail.com>
2015-11-04 22:21:37 -0300

    basic structure

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+def read(*paths):
+    """ read files """
+    with open(os.path.join(*paths), 'r') as filename:
+        return filename.read()
+
+
+install_requires = read('requirements.txt').splitlines()
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="pypptx",
+    version="0.1.0",
+    description="Create a pptx from plain text",
+    license="MIT",
+    author="JuanPablo AJ",
+    author_email="jpabloaj@gmail.com",
+    packages=find_packages(),
+    install_requires=install_requires,
+    long_description=long_description,
+    packages=['pypptx'],
+    test_suite="tests",
+    entry_points={
+        'console_scripts': [
+            'pptx=pypptx:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
