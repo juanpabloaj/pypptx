ab92ce1be3fa31884da9834803bc661c75402d67
JuanPablo <jpabloaj@gmail.com>
2015-11-06 23:15:10 -0300

    bump to 0.2.0
    * change layout support

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except IOError:
 
 setup(
     name="pypptx",
-    version="0.1.1",
+    version="0.2.0",
     description="Create a pptx from plain text",
     license="MIT",
     author="JuanPablo AJ",
